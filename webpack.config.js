aa7538a5197bb8573a0dcd77cedbd63677d4f0c4
Jari van der Kaap <jarivdkaap@gmail.com>
2020-07-03 19:17:57 +0200

    Added socials at the top, better title backgrounds

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,6 @@ module.exports = {
       },
       { test: /\.(woff|woff2)$/, loader: 'url-loader?prefix=font/&limit=5000' },
       { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream' },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml' },
     ],
   },
   plugins: [
